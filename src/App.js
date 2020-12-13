f8cd0b0d9788e996faaca98e43d43e2cf883dee9
Paul Farcasanu <pfarcasanu@gmail.com>
2020-12-13 15:17:05 -0600

    removed fucntions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Game from './Components/Game';
-import Add from './Components/Add';
 import WordList from './Components/WordList';
-import Contribute from './Components/Contribute';
 import Header from './Components/Header';
 
 const App = () => (
   <HashRouter>
     <Header />
     <Switch>
-      <Route exact path="/contribute">
-        <Contribute />
-      </Route>
-      <Route exact path="/add">
-        <Add />
-      </Route>
       <Route exact path="/list">
         <WordList />
       </Route>
